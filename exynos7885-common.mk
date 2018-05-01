33b9086683650eb577e729f7d3e1082ab1722969
Jesse Chan <jc@lineageos.org>
2018-04-30 23:02:55 -0700

    exynos7885: overlay: enable option for full screen aspect ratio
    Change-Id: Ica01880ce444313ae08151e6cfdcf5ef71a523b6
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- exynos7885-common.mk
+++ exynos7885-common.mk
@@ -148,6 +148,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
