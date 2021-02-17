8e113349a37815650a21d507304ec3de0aabccf5
Mikhail Naganov <mnaganov@google.com>
2021-02-17 20:36:38 +0000

    exynos7885: Switch audio HAL to V7
    Updated audio policy manager configuration files to comply
    with the changes in the XML schema. This was done automatically
    by running the following commands:
    
      hardware/interfaces/audio/7.0/config/update_audio_policy_config.sh \
          device/samsung/universal7904-common/configs/audio/audio_policy_configuration.xml 6.0
    
    Bug: 142480271
    Test: atest VtsHalAudioV7_0TargetTest
    Test: atest VtsHalAudioEffectV7_0TargetTest
    Change-Id: I55d3728c809d27e58d5d496a1c7d637050dde53a
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- exynos7885-common.mk
+++ exynos7885-common.mk
@@ -24,16 +24,16 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
+    android.hardware.audio@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.effect@7.0-impl \
     libtinycompress \
     audio.r_submix.default \
     audio.usb.default
 
 # Audio Configs
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
