60a53637b1c866ad7daa6a37817ccbf403c0abe2
Tim Zimmermann <tim@linux4.de>
2021-04-07 16:20:39 +0200

    exynos7885: get access to all cameras
    * Possible camera IDs are from /vendor/etc/floating_feature.xml
    
    Change-Id: I5fb0fb57f5ea85c0c8ead5ddec2dba3cf0277f94

--- exynos7885-common.mk
+++ exynos7885-common.mk
@@ -50,7 +50,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.5-service
+    android.hardware.camera.provider@2.5-service.samsung
 
 # DRM
 PRODUCT_PACKAGES += \
