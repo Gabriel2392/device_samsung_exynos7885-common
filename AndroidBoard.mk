568d70d6c993d5f78d6a93344f41bc3e27d51dc0
Tim Zimmermann <tim@linux4.de>
2021-05-25 15:52:56 +0200

    exynos7885: use INSTALLED_RADIOIMAGE_TARGET for building dtb
    * So we don't need to duplicate dtb.img
    
    Change-Id: Iff858e33ab8f44bd181b4e6f0f7f5b9be07eeb7b

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -14,24 +14,19 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
 MKDTIMG    := $(HOST_OUT_EXECUTABLES)/mkdtimg$(HOST_EXECUTABLE_SUFFIX)
 KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
 DTB_DIR    := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/exynos
 DTB_CFG    := $(COMMON_PATH)/configs/kernel/exynos7885.cfg
 
-INSTALLED_DTBIMAGE_TARGET := $(PRODUCT_OUT)/dtb/dtb.img
+INSTALLED_DTBIMAGE_TARGET := $(PRODUCT_OUT)/dtb.img
 
-$(INSTALLED_DTBIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(MKDTIMG) $(AVBTOOL)
+$(INSTALLED_DTBIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(MKDTIMG)
 	$(hide) echo "Building dtb.img"
-	$(hide) mkdir -p $(dir $@)
 	$(hide) $(MKDTIMG) cfg_create $@ $(DTB_CFG) -d $(DTB_DIR)
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_DTBIMAGE_PARTITION_SIZE),raw)
 
 .PHONY: dtbimage
 dtbimage: $(INSTALLED_DTBIMAGE_TARGET)
 
-$(call add-radio-file,../../../../$(INSTALLED_DTBIMAGE_TARGET))
+INSTALLED_RADIOIMAGE_TARGET += $(INSTALLED_DTBIMAGE_TARGET)
