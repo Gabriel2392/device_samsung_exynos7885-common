18f3ccb77aba3718d3a66cf821df57780bd83427
Simon1511 <me@simon1511.de>
2024-01-18 19:18:02 +0100

    exynos7885: libsec-ril: Always return an empty value for GET_SMSC_ADDRESS
    * Make parameter of type RilData (second last one) passed to SecRil::RequestComplete()
      in OnGetSmscAddressDone() be NULL, so that the framework passes an empty value when sending SMS
    * Before U this value was not being used, on U the value returned is "SMSC",format_specifier
      which is considered invalid when passed back to RIL for sending SMS, however when an empty value
      is passed RIL will select the default SMSC, like it did on T and sending SMS works again.
    
    Change-Id: I608be406969d7fa58f15ef36340aff4ad1ce9fd0
    Co-authored-by: Tim Zimmermann <tim@linux4.de>

--- extract-files.sh
+++ extract-files.sh
@@ -72,6 +72,9 @@ function blob_fixup() {
 	    ;;
 	vendor/bin/hw/rild | vendor/lib*/libsec-ril*.so)
 	    "$PATCHELF" --replace-needed libril.so libril-samsung.so "$2"
+	    # Pass an empty value to SecRil::RequestComplete in OnGetSmscAddressDone
+	    xxd -p -c0 "${2}" | sed "s/600e40f9820c805224008052e10315aae30314aa/600e40f9820c805224008052e10315aa030080d2/g" | xxd -r -p > "${2}".patched
+	    mv "${2}".patched "${2}"
 	    ;;
 	vendor/lib*/libskeymaster4device.so)
             "${PATCHELF}" --replace-needed libcrypto.so libcrypto-v33.so "${2}"
