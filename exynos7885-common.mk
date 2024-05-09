5e63afef028ce6a43e002c92a1708d3895d0b7d5
Tim Zimmermann <tim@linux4.de>
2024-05-09 06:48:24 +0200

    exynos7885: Use pixel power HAL directly
    * Samsung one is outdated and we didn't do any meaningful changes there
    
    Change-Id: I65378563396d12ee76e7f2b34105988c7e66cf38

--- exynos7885-common.mk
+++ exynos7885-common.mk
@@ -252,7 +252,7 @@ endif
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.samsung-libperfmgr
+    android.hardware.power-service.pixel-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
