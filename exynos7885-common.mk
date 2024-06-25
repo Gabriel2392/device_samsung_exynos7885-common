b4e271ab3826041e77daf250407b53d76b0d9845
K9100ii <k9100ii.ki@gmail.com>
2024-06-25 12:48:13 +0100

    exynos7885: Use OSS vendor.samsung.hardware.radio@2.1 from hardware/samsung
    Change-Id: I6fd406a6f8d3e703b7824355c31e6ab4dfcb6306

--- exynos7885-common.mk
+++ exynos7885-common.mk
@@ -271,6 +271,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.4.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
+    vendor.samsung.hardware.radio@2.1.vendor \
     libnetutils.vendor \
     libsqlite.vendor
 
