97118aa31ad2639d78210aab713faf7099df16ad
Tim Zimmermann <tim@linux4.de>
2024-07-30 05:57:11 +0200

    exynos7885: Switch to hardware/samsung FCM
    Change-Id: Id915542a0b5c4d27e488cae0d97ed36d299a428b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,6 +82,7 @@ TARGET_KEYMASTER_VARIANT := samsung
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml \
     $(COMMON_PATH)/device_framework_matrix.xml
 
