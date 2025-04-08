d23987e1b0a6898b81e88542ee3bd7e5f36cc983
royna2544 <whiteshell2544@naver.com>
2025-04-08 10:19:18 +0900

    exynos7885: Import overlay from old exynos7885-common
    - And clean it up

--- exynos7885-common.mk
+++ exynos7885-common.mk
@@ -131,6 +131,9 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
