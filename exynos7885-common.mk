2a0c154f39cbef1437e275a1910d7a67d5792568
royna2544 <whiteshell2544@naver.com>
2025-04-08 16:39:34 +0900

    exynos7885: rootdir: Move ramdisk fstab to vendor/etc

--- exynos7885-common.mk
+++ exynos7885-common.mk
@@ -101,7 +101,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc \
-    $(LOCAL_PATH)/rootdir/etc/fstab.exynos7885:$(TARGET_COPY_OUT_RAMDISK)/fstab.$(TARGET_SOC)
+    $(LOCAL_PATH)/rootdir/etc/fstab.exynos7885:$(TARGET_COPY_OUT_RAMDISK)/vendor/etc/fstab.$(TARGET_SOC)
 
 # Keymaster
 PRODUCT_PACKAGES += \
