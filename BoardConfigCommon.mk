3ac3fa3db3c482c8fec5c9582c4237c94a87e19e
Gabriel2392 <gabriel824m@gmail.com>
2025-09-14 23:22:41 -0300

    exynos7885-common: Use 32bit camera for a10

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,8 +31,10 @@ BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 # Camera
 ifneq ($(TARGET_DEVICE),a10)
 $(call soong_config_set,samsungCameraVars,extra_ids,50)
-endif
 $(call soong_config_set,samsungCameraVars,usage_64bit,true)
+else
+$(call soong_config_set,samsungCameraVars,camera_32bit,true)
+endif
 
 # Display
 ifeq ($(call has_feature,aod),false)
